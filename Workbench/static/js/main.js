ff80b1c76491353466e289c5d858f1a6dd92047f
Carl Schéle <carl_schele@hotmail.com>
2020-05-20 22:26:07 +0200

    adding js

---
+++ Workbench/static/js/main.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function() {
+    $("span.random-font-color").each(function (index) {
+  
+        var text = Array.from($(this).text());
+
+        var s = "";
+        text.forEach(function (entry) {
+            s += '<span class="lavender">ccccc</span>';
+        });
+
+        console.log(s);
+
+    });
+
+});
